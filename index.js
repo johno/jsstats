5b64894ff4912ce5d3f63d6fa95a1964ca78bfdb
John Otander <johnotander@gmail.com>
2015-08-14 14:43:21 -0600

    Add basic declaration and statement aggregates

--- index.js
+++ index.js
@@ -9,18 +9,29 @@ module.exports = function jsstats(jsString) {
   }
 
   var ast = babel.transform(jsString).ast
+  // console.log(ast.program)
 
   return {
+    declarations: {
+      total: _.select(ast.program.body, function (obj) {
+        return ['VariableDeclaration', 'FunctionDeclaration'].indexOf(obj.type) !== -1
+      }).length
+    },
+    expressions: {
+      total: _.select(ast.program.body, function (obj) {
+        return obj.type === 'ExpressionStatement'
+      }).length
+    },
     variables: {
       lets: Object.keys(ast.program._letReferences).length
     },
     comments: {
       total: ast.comments.length,
       block: _.select(ast.comments, function (obj) {
-        return obj.type == 'CommentBlock'
+        return obj.type === 'CommentBlock'
       }).length,
       line: _.select(ast.comments, function (obj) {
-        return obj.type == 'CommentLine'
+        return obj.type === 'CommentLine'
       }).length
     }
   }
